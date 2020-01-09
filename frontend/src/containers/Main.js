8cadfb9eb75310f44fc79837b80f2d790fad9948
Refugee-Studio <refugeebase@gmail.com>
2020-01-09 16:47:23 -0500

    add resized pictures

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -27,28 +27,28 @@ class Main extends Component {
                         <div className='g1'>
                           <Link to='/bloood-pressure'>
                             <h5 className="card-title">Blood Pressure</h5>
-                            <img src={'https://storage.needpix.com/rsynced_images/cardiac-156059_1280.png'} alt="bloodPressure" className="img_size"/>
+                            <img src={'https://sun9-19.userapi.com/c858528/v858528370/a7af4/op-Kp0298FI.jpg'} alt="bloodPressure" className="img_size"/>
                           </Link>
                         </div>
 
                         <div className='g1'>
                           <Link to='/cholesterol'>
                             <h5 className="card-title">Cholesterol</h5>
-                            <img src={'http://www.picserver.org/highway-signs2/images/cholesterol.jpg'} alt="cholesterol" className="img_size"/>
+                            <img src={'https://sun9-38.userapi.com/c858528/v858528370/a7ae6/il7HxSFAKts.jpg'} alt="cholesterol" className="img_size"/>
                           </Link>
                         </div>
 
                         <div className='g1'>
                           <Link to='/metabolic-panel'>
                             <h5 className="card-title">Metabolic Panel</h5>
-                            <img src={'https://st4.depositphotos.com/4177785/27492/v/450/depositphotos_274920202-stock-illustration-healthy-liver-color-icon-human.jpg'} alt="metabolism" className="img_size"/>
+                            <img src={'https://sun9-60.userapi.com/c858528/v858528370/a7ad1/_EOlyRrYd4E.jpg'} alt="metabolism" className="img_size"/>
                           </Link>
                         </div>
 
                         <div className='g1'>
                           <Link to='/vitamine-panel'>
                             <h5 className="card-title">Vitamine Panel</h5>
-                            <img src={'https://hhp-blog.s3.amazonaws.com/2018/02/iStock-505820296.jpg'} alt="cholesterol" className="img_size"/>
+                            <img src={'https://sun9-61.userapi.com/c858528/v858528370/a7adf/7HHhUqX_uRc.jpg'} alt="cholesterol" className="img_size"/>
                           </Link>
                         </div>
                       </div>
@@ -64,14 +64,14 @@ class Main extends Component {
                         <div className='g1'>
                           <Link to='/flue-shot'>
                             <h5 className="card-title">Flue Shots</h5>
-                            <img src={'https://obamawhitehouse.archives.gov/sites/default/files/image/image_file/h1n1_shot.jpg'} alt="flu shot" className="img_size"/>
+                            <img src={'https://sun9-9.userapi.com/c858528/v858528370/a7ad8/IEGEuf_mmts.jpg'} alt="flu shot" className="img_size"/>
                           </Link>
                         </div>
 
                         <div className='g1'>
                           <Link to='/blood-presure-test'>
                             <h5 className="card-title">Blood pressure tests</h5>
-                            <img src={'https://cdn.pixabay.com/photo/2019/01/15/21/19/blood-3934879_960_720.jpg'} alt="blood pressure test" className="img_size"/>
+                            <img src={'https://sun9-49.userapi.com/c858528/v858528370/a7aed/k9EbqbXrOEQ.jpg'} alt="blood pressure test" className="img_size"/>
                           </Link>
                         </div>
                       </div>
