e4b41d329097873b9926cc923fece36004acf77f
pavel ilin <refugeebase@gmail.com>
2020-07-08 10:53:48 -0400

    chartjs init

--- client/src/components/BloodPressureDiagram.js
+++ client/src/components/BloodPressureDiagram.js
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from 'react'
-// import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalGridLines, DiscreteColorLegend } from 'react-vis';
-import { useSelector } from 'react-redux'
+import React, { useRef, useEffect } from 'react';
+import Chart from 'chart.js';
+import { useSelector } from 'react-redux';
 import '../assets/index.css';
 
 let combinedArray = {
@@ -9,13 +9,6 @@ let combinedArray = {
   dataPuls: [],
   dataStress: []
  }
-
- const bloodPressureItems = [
-   {title: 'Systolic', color: 'red', strokeWidth: 6},
-   {title: 'Diastolic', color: 'blue', strokeWidth: 6},
-   {title: 'Puls', color: 'green', strokeWidth: 6},
-   {title: 'Stress level', color: 'black', strokeWidth: 6}
- ]
 
  const testResults = (bloodPressures) => {
    combinedArray = {
@@ -47,78 +40,33 @@ let combinedArray = {
 const BloodPressureDiagram = () => {
     let bloodPressures = useSelector(state => state.blood_pressures);
     testResults(bloodPressures)
-    // const svgRef = useRef()
+    const chartRef = useRef()
+    
+    useEffect(() => {
+      const myChartRef = chartRef.current.getContext("2d");
 
-   
+      new Chart(myChartRef, {
+        type: 'line',
+        data: {
+          labels: ["Jan", "Feb", "March"],
+          datasets: [
+              {
+                  label: "Systolic",
+                  data: [86, 67, 91],
+              }
+          ]
+      },
+      options: {
+          //Customize chart options
+      }
+      });
+    })
 
     return  (
       <div>
-         comming soon
+         <canvas id="myChart" ref={chartRef} />
       </div>
     )
   }
 
 export default BloodPressureDiagram
-
-
-{/* <>
-          <XYPlot
-            width={500}
-            height={500}>
-            <VerticalGridLines />
-            <HorizontalGridLines />
-
-            <LineSeries
-              title="Systolic"
-              style={{stroke: 'red', strokeWidth: 3}}
-              data={combinedArray.dataSystolic}/>
-            <XAxis title="Time"/>
-            <YAxis />
-
-            <LineSeries
-                title="Diastolic"
-                style={{stroke: 'blue', strokeWidth: 3}}
-                data={combinedArray.dataDiastolic}/>
-            <YAxis />
-
-            <LineSeries
-                title="Pulse"
-                style={{stroke: 'green', strokeWidth: 3}}
-                data={combinedArray.dataPuls}/>
-            <YAxis />
-
-            <LineSeries
-                title="Stress level"
-                style={{stroke: 'black', strokeWidth: 3}}
-                data={combinedArray.dataStress}/>
-            <YAxis />
-
-            <DiscreteColorLegend orientation="horizontal" width={300} items={bloodPressureItems} />
-
-          </XYPlot>
-          </> */}
-
-
-
-        //   useEffect(() => {
-        //     const svg = select(svgRef.current);
-        //     const myLine = line()
-        //       .x((value, index) => index * 10)
-        //       .y(value => value)
-            
-        //     svg.selectAll('path')
-        //       .data([data])
-        //       .join('path')
-        //       .attr('d', value => myLine(value))
-        //       .attr('fill', 'none')
-        //       .attr('stroke', 'blue')
-        //       .attr("stroke-width", 1.5)
-        //       .attr("stroke-linejoin", "round")
-        //       .attr("stroke-linecap", "round")
-        //   }, [data])
-          
-        // return(
-        //       <div>
-        //         <svg ref={svgRef} height='100%' width='30%'></svg>
-        //       </div>
-        //   )
